299d2398acd5a9ecb1204589a2891e4731fd6b1a
Moi <a.mostin@students.ephec.be>
2020-07-01 19:55:37 +0200

    bouton "ajout employé"

--- src/view/GerantVue.java
+++ src/view/GerantVue.java
@@ -6,29 +6,52 @@ package view;
 import java.awt.BorderLayout;
 
 import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 /**
  * @author Moi
  *
  */
 public class GerantVue {
+	private JFrame gerantFrame;
+	private JPanel gerantTextContent = new JPanel();
+	private JLabel gerantMessage = new JLabel("Bienvenue Gérant");
+	private JButton nouvelEmploye = new JButton("Ajouter un employé");
 
+	private JTextField mdp = new JTextField();
+	private JButton connexion = new JButton("Connexion");
+	private JComboBox<String> userType;
 	/**
 	 * 
 	 */
 	public GerantVue() {
-		JFrame gerantFrame;
-		JPanel gerantTextContent = new JPanel();
-		JLabel gerantMessage = new JLabel("Bienvenue Gérant");
+
 		
 		gerantFrame = new JFrame("Rentacar");
 		gerantTextContent.setLayout(new BoxLayout(gerantTextContent, BoxLayout.Y_AXIS));
 		gerantTextContent.add(gerantMessage);
 		
 		gerantFrame.add(gerantTextContent, BorderLayout.NORTH);
+		/*
+		JPanel fieldZone = new JPanel();
+		fieldZone.setLayout(new BoxLayout(fieldZone, BoxLayout.X_AXIS));
+		String[] roles = {"employé", "mécanicien"};
+		userType = new JComboBox<>(roles);
+		fieldZone.add(userType);
+		JLabel mdpLabel = new JLabel("mdp : ");
+		fieldZone.add(mdpLabel);
+		fieldZone.add(mdp);
+		gerantFrame.add(fieldZone, BorderLayout.CENTER);
+		*/
+		
+		JPanel panelbuttons = new JPanel();
+		panelbuttons.add(nouvelEmploye);
+		gerantFrame.add(panelbuttons, BorderLayout.SOUTH);
 		
 		gerantFrame.pack();
 		gerantFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
